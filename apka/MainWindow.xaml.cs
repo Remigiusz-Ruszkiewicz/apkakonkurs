9aadd40d879f022518d40f98bd8d4e113d6b4cf6
jagoda2309 <47605818+jagoda2309@users.noreply.github.com>
2019-10-23 22:50:03 +0200

    Add project files.

---
+++ apka/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace apka
+{
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public void check()
+        {
+            var rows = File.ReadAllLines("myfile.csv").Select(l => l.Split(',').ToArray()).ToArray();
+            MessageBox.Show(rows[0][0]);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var rows = File.ReadAllLines("myfile.csv").Select(l => l.Split(',').ToArray()).ToArray();
+            foreach (var item in rows)
+            {
+                var i = rows.ToArray().ToString();
+                if (input.Text == i[0][0])
+                {
+                    MessageBox.Show("OK");
+                }
+            }
+           
+            check();
+        }
+    }
+}
